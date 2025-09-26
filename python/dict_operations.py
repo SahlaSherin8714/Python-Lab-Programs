8513e3846db324486a52a9df30026f978f241889
SahlaSherin8714 <sahlasherin8714@gmail.com>
2025-09-26 12:15:28 +0530

    Update dict_operations.py

--- python/dict_operations.py
+++ python/dict_operations.py
@@ -10,3 +10,17 @@ d=dict()
 for i in range(1,n+1):
     d[i]=i*i
 print(d)
+
+#summ of all items in dictionary
+dic1=dict(m1=40,m2=30,m3=50)
+sum1=0
+for i in dic1.values():
+    sum1+=i
+print(sum1)
+
+#product of all items in dictionary
+dic=dict(a=1,b=2,c=6)
+prod=1
+for i in dic.values():
+    prod*=i
+print(prod)
