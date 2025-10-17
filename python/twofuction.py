8fc68a9ec1717e6ed1c17cc2d7a7f8821ca85aae
SahlaSherin8714 <sahlasherin8714@gmail.com>
2025-10-17 12:09:11 +0530

    twofuction.py

--- python/twofuction.py
+++ python/twofuction.py
@@ -1,26 +1,24 @@
 #Create a function to read a number (minimum 4 digits) from the user and find the reverse of the 
 #number using loop in another function and display both number and reverse of the number
 
+def read_number():
+    while True:
+        n = input("Enter a number (min 4 digits): ")
+        if n.isdigit():
+            if len(n) >= 4:
+                return int(n)
+            else:
+                print("Please enter min 4 digits.")
+        else:
+            print("Invalid input, please enter digits only.")
+
 def reverse_number(num):
-    if num < 0:
-        return "Enter a positive number"
-    
     rev = 0
     while num > 0:
-        last_dig = num % 10
-        rev = rev * 10 + last_dig
-        num = num // 10
+        rev = rev * 10 + num % 10
+        num //= 10
     return rev
 
-def main():
-    number = input("Enter a number with minimum 4 digits: ")
-    if len(number) < 4:
-        print("Please enter a valid number with at least 4 digits.")
-        return 
-    
-    number = int(number)
-    reversed_number = reverse_number(number)
-    print(f"Reversed number: {reversed_number}")
-
-if __name__ == "__main__":
-    main()
+n = read_number()
+print("Original:", n)
+print("Reversed:", reverse_number(n))
